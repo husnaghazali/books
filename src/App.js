57f25fe84daf9ec13e2b6e2601656d7ed4920e7a
husna <husnazghazali@gmail.com>
2024-03-05 11:32:46 +0100

    first component create book

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './index.css';
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+
+function App() {
+    const [books, setBooks] = useState ([]);
+
+    const createBook = (title) =>{
+        
+    };
+
+    return <div>
+         <BookCreate onCreate = {createBook} />
+    </div>;
+}
+
+export default App;
